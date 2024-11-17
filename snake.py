55aaecc0f899b31568a5a008c3721db6e57d8d1f
iveri99 <lukacho.iv@gmail.com>
2024-11-17 11:26:33 +0000

    Creating Snake class

--- snake.py
+++ snake.py
@@ -12,5 +12,15 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-screen = pygame.display.set_mode((800,800)) # Set up screen/display
+screen = pygame.display.set_mode((HEIGHT,WIDTH)) # Set up screen/display
+pygame.display.set_caption("PySnake")
+clock = pygame.time.Clock()
 
+class Snake:
+    def __init__(self): # Initialise Snake object
+        self.body = [[100,100], [90,100], [80,100]]
+        self.direction = "RIGHT"
+
+snake = Snake()
+print(snake.body)
+print(snake.direction)
