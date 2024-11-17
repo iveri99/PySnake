4540bce091783199a81b941bc0ae0de22480fcd8
iveri99 <lukacho.iv@gmail.com>
2024-11-17 10:40:29 +0000

    .

---
+++ snake.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+pygame.init() # Initialise pygame
+
+# Constants
+HEIGHT = 600
+WIDTH = 800 
+CELL_SIZE = 20 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+screen = pygame.display.set_mode((800,800)) # Set up screen/display
+
