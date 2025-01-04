1b60fe59fa322395866155590aeab1936b897298
iveri99 <lukacho.iv@gmail.com>
2025-01-04 18:41:49 +0000

    Adding more to the Snake class, simulating movement

--- snake.py
+++ snake.py
@@ -20,7 +20,38 @@ class Snake:
     def __init__(self): # Initialise Snake object
         self.body = [[100,100], [90,100], [80,100]]
         self.direction = "RIGHT"
+    def move(self):
+        # Getting position of head
+        head_x, head_y = self.body[0]
+        
+        # Update head position based on direction
+        if self.direction == "RIGHT":
+            new_head = (head_x + 10, head_y)
+        elif self.direction == "LEFT":
+            new_head = (head_x - 10, head_y)
+        elif self.direction == "UP":
+            new_head = (head_x, head_y + 10)
+        elif self.direction == "DOWN":
+            new_head = (head_x, head_y - 10)
+
+        # Add new head and remove last part to create movement
+        self.body.insert(0, new_head)
+        self.body.pop()
+    
+    
+
+
+
+
 
 snake = Snake()
-print(snake.body)
-print(snake.direction)
+
+screen.fill((255,245,200))
+pygame.display.flip()
+
+running = True
+while running: # Game loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
